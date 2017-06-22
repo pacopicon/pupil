73962dd2045f1161362c3a78692ec2dc15702288
Paco <palmtreerooskee@gmail.com>
2017-06-22 07:25:24 -0400

    On third Wes Bos video, 9:33

---
+++ src/components/StorePicker.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+  // ReactDom.render() {
+  render() {
+    return <p>Hello from nose picker</p>
+  }
+}
+
+export default StorePicker;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+// import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+// import './index.css';
+import StorePicker from './components/StorePicker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+// ReactDom.render(...)
+render(<StorePicker/>, document.querySelector('#main'));
