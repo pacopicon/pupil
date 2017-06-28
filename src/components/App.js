23d7dbc1b569616826f3b8d0f46f3b248944294e
Paco <palmtreerooskee@gmail.com>
2017-06-28 07:42:04 -0400

    video 20 4:05)

--- src/components/App.js
+++ src/components/App.js
@@ -91,7 +91,7 @@ class App extends React.Component {
         <Order
           fishes={this.state.fishes} order={this.state.order}
           params={this.props.match.params}/>
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} fishes={this.state.fishes}/>
       </div>
     )
   }
