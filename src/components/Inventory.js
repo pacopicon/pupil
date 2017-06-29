41f4913707e237b3a6a1683eaadfc768f524302d
Paco <palmtreerooskee@gmail.com>
2017-06-29 18:17:21 -0400

    transferring work from macBook: authentication is DONE

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,12 +1,103 @@
 import React from 'react';
 import AddFishForm from './AddFishForm';
 import PropTypes from 'prop-types';
+import firebase from 'firebase';
+import { database } from 'firebase';
+import rebase from '../base';
 
 class Inventory extends React.Component {
+
   constructor() {
     super();
     this.renderInventory = this.renderInventory.bind(this);
+    this.renderLogin = this.renderLogin.bind(this);
+    this.authenticate = this.authenticate.bind(this);
+    this.logout = this.logout.bind(this);
+    this.authHandler = this.authHandler.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      uid: null,
+      owner: null
+    }
+  }
+
+  componentDidMount() {
+    rebase.app.auth().onAuthStateChanged((user, error) => {
+      if(user) {
+        this.authHandler({user});
+      }
+    });
+  };
+
+  // const app = rebase.app;
+  // const base = rebase.base;
+
+  authenticate(provider) {
+    console.log(`Trying to log in with ${provider}`);
+
+    rebase.app.auth().signInWithPopup(provider).then((result) => {
+      const token = result.credential.accessToken;
+      const user = result.user;
+      console.log('token = ' + token + ', user = ' + user);
+      const authData = {
+        token: token,
+        user: user
+      }
+      this.authHandler(authData);
+    });
+
+  }
+
+  logout() {
+    rebase.app.auth().signOut().then(() => {
+      console.log("should have been logged out");
+    });
+    this.setState({ uid: null });
+  }
+
+  authHandler(authData) {
+    console.log('authData = ', authData);
+    if(!authData || typeof authData === "undefined") {
+      console.log('did not receive authData for this signup session');
+      return;
+    }
+
+    // grab the store info
+    const storeRef = database(rebase.app).ref(this.props.storeID);
+
+    // query the firebase once for the store data
+    storeRef.once('value', (snapshot) => {
+      const data = snapshot.val() || {};
+
+      // claim it as our own if there is no owner already
+
+      if(!data.owner) {
+        storeRef.set({
+          owner: authData.user.uid
+        });
+      }
+
+      this.setState({
+        uid: authData.user.uid,
+        owner: data.owner || authData.user.uid
+      });
+    });
+  }
+
+  renderLogin() {
+    const fbprovider = new firebase.auth.FacebookAuthProvider();
+    const twprovider = new firebase.auth.TwitterAuthProvider();
+    const ghprovider = new firebase.auth.GithubAuthProvider();
+
+    return (
+      <nav className="login">
+        <h2>Inventory</h2>
+        <p>Sign in to manage your store's inventory</p>
+        <button className="github" onClick={() => this.authenticate(ghprovider)}>Log In with Github</button>
+        <button className="facebook" onClick={() => this.authenticate(fbprovider)}>Log In with Facebook</button>
+        <button className="twitter" onClick={() => this.authenticate(twprovider)}>Log In with Twitter</button>
+      </nav>
+    )
   }
 
   handleChange(e, key) {
@@ -18,6 +109,7 @@ class Inventory extends React.Component {
     }
     this.props.updateFish(key, updatedFish);
   }
+
   renderInventory(key) {
     const fish = this.props.fishes[key];
     return (
@@ -36,9 +128,25 @@ class Inventory extends React.Component {
   }
 
   render() {
+    // the long way: const logout = <button onClick={ () => this.logout()}>Log Out!</button>;
+    const logout = <button onClick={this.logout}>Log Out!</button>;
+    // check if they are not logged in at all
+    if(!this.state.uid) {
+      return <div>{this.renderLogin()}</div>
+    }
+
+    if(this.state.uid !== this.state.owner) {
+      return (
+        <div>
+          <p>Sorry you aren't the owner of the store!</p>
+          {logout}
+        </div>
+      )
+    }
     return(
       <div>
         <h2>Inventory</h2>
+        {logout}
         {Object.keys(this.props.fishes).map(this.renderInventory)}
         <AddFishForm addFish={this.props.addFish} />
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
@@ -52,7 +160,8 @@ Inventory.propTypes = {
   updateFish: PropTypes.func,
   removeFish: PropTypes.func,
   addFish: PropTypes.func.isRequired,
-  loadSamples: PropTypes.func.isRequired
+  loadSamples: PropTypes.func.isRequired,
+  storeID: PropTypes.string.isRequired
 };
 
 export default Inventory;
